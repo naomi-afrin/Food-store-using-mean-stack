328301c46c0b7c4430dc5034036ece04d02cc0d3
Naomi_Jalil <110171806+Naomi-Jalil@users.noreply.github.com>
2024-06-04 09:20:01 +0600

    created food model

---
+++ frontend/src/app/shared/models/food.ts
@@ -0,0 +1,13 @@
+export class Food{
+    // required fileds defined by !
+    // optional fields defined by ?
+    id!: string;
+    name!: string;
+    price!: number;
+    tas?: string[];
+    favourite!: boolean;
+    stars!: number;
+    imaeUrl!: string;
+    origins!: string[];
+    cookTime!: string;
+}
